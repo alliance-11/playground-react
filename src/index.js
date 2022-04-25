9aacf79f4a04ae2e2d1fa7512357d1fa8565991e
losrobbos <losrobbos@web.de>
2022-04-25 13:32:31 +0200

    Polishing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App-fullname';
-// import { HW } from './App-fullname';
 import App from './App';
 
 ReactDOM.render(
